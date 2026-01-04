97889c77c23e0af92239a6b92751195b4bd73b3a
itayshr <itayshr8@gmail.com>
2026-01-04 15:48:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-# הגדרת הרשאות (Intents)
+# הגדרת הרשאות
 intents = discord.Intents.default()
 intents.members = True          
 intents.message_content = True  
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'--- הבוט {bot.user.name} מחובר! ---')
+    print(f'--- הבוט {bot.user.name} מוכן לעבודה! ---')
 
 @bot.event
 async def on_member_join(member):
@@ -30,22 +30,23 @@ async def on_member_join(member):
             title="שלום רב !!",
             description=f"<@{member.id}>\n\n"
                         f"**ברוך/ה הבא/ה לשרת ה Fivem Roleplay של קהילת**\n"
-                        f"**{guild.name}**\n\n"
+                        f"**Gamers-Israel | 🇬**\n\n"
                         f"אנו ממליצים לך לעבור על [חוקי השרת](https://google.com) לפני כניסתך לשרת המשחק "
                         f"בכדי לאפשר עבורך ועבור שאר השחקנים חווית משחק מהנה ואיכותית יותר\n\n"
                         f"**שיהיה בהצלחה !! ❤️**",
             color=discord.Color.blue()
         )
         
-        # הגדרת כותרת עליונה עם שם השרת והלוגו שלו
-        embed.set_author(name=f"{guild.name} | Serious Roleplay", icon_url=guild.icon.url if guild.icon else None)
-        
-        # הגדרת התמונה בצד (Thumbnail) כתמונת השרת
+        # לוגו השרת בצד ימין למעלה
         if guild.icon:
+            embed.set_author(name=f"{guild.name} | Serious Roleplay", icon_url=guild.icon.url)
             embed.set_thumbnail(url=guild.icon.url)
         
-        # פוטר עם שם השרת
-        embed.set_footer(text=guild.name, icon_url=guild.icon.url if guild.icon else None)
+        # --- הוספת הבאנר של GameLife מתחת לכיתוב (התמונה הגדולה) ---
+        embed.set_image(url="https://storage.googleapis.com/public-assets-x/image_10a2a3.jpg")
+        
+        # פוטר בתחתית
+        embed.set_footer(text="GAMERS ISRAEL", icon_url=guild.icon.url if guild.icon else None)
 
         await channel.send(embed=embed)
 
@@ -54,5 +55,4 @@ async def on_member_join(member):
 async def test(ctx):
     await on_member_join(ctx.author)
 
-# הרצת הבוט
 bot.run(os.getenv("DISCORD_TOKEN"))
